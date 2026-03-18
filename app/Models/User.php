2eac5631cd7060547b3834f03a2607a13abaae2d
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-18 12:28:55 -0300

    refactor: adiciona o campo surname na model de User.

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,12 +10,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+// #[Fillable(['name', 'surname','email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = ['name', 'surname','email', 'password'];
 
     /**
      * Get the attributes that should be cast.
