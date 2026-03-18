c777f33b3fe49a573aea5a7a4cdbfee9c1d1f47c
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-18 12:29:35 -0300

    refactor: controller com primeiros exemplos. sail artisan make:controller HomeController.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
+
+class HomeController extends Controller
+{
+
+    public function index($name='Mundo!!!') {
+
+        return view('olamundo',['name'=>$name]);
+    }
+
+    public function list() {
+
+        $userList  = User::all();
+       // dd($userList);
+       return View::make('user_list',compact('userList'));
+    }
+}
