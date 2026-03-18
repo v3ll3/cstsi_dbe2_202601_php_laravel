fec5925a50b2ce099171c0ced9d34392b9becdf1
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-18 09:16:58 -0300

    feat: criação projeto laravel com composer create-project composer create-project laravel/laravel topico02

--- src/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use CSTSI\Dbe2\app\controllers\{ProdutoController, HomeController};
-
-$routes = [
-    '/' => HomeController::class,
-    'produtos' => ProdutoController::class
-];
--- src/public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once '../../vendor/autoload.php';
-
-use CSTSI\Dbe2\app\core\App;
-
-App::init();
