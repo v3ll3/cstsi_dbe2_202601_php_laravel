5ad659a57ed02a25eaa98fe1c7a9091edd5475a1
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-25 10:56:57 -0300

    feat: criação do template de detalhes de produto. Template blade show. sail artisan make:view produtos.show

---
+++ resources/views/produtos/show.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Produtos</title>
+</head>
+<body>
+    @if ($produto)
+        <h1>{{ $produto->nome }}</h1>
+        <p>{{ $produto->descricao }}</p>
+        <ul>
+            <li>Quantidade: {{ $produto->qtd_estoque }}</li>
+            <li>Preço: {{ $produto->preco }}</li>
+            <li>Importado: {{ $produto->importado ? 'Sim' : 'Não' }}</li>
+        </ul>
+    @else
+        <p>Produtos não encontrados! </p>
+    @endif
+    <a href="/produtos">&#9664;Voltar</a>
+</body>
+</html>
