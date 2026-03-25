eda7ba18efb6fb3b524e6e5332400b3dab8ab604
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-25 10:58:15 -0300

    feat: link para a rota de criação de produtos

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -8,6 +8,7 @@
 </head>
 <body>
     <h1>Produtos</h1>
+    <a href="/produto" style="display: inline">&#43;&nbsp;Novo Produto</a>
     @if ($produtos->count()>0)
     <table>
         <thead>
@@ -22,7 +23,7 @@
         <tbody>
             @foreach($produtos as $produto)
             <tr>
-                <td>{{$produto->id}}</td>
+                <td><a href="/produtos/{{$produto->id}}">{{$produto->id}}</a></td>
                 <td>{{$produto->nome}}</td>
                 <td>{{$produto->qtd_estoque}}</td>
                 <td>{{$produto->preco}}</td>
