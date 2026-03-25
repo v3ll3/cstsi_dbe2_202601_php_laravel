04326b84218eb67d7b5658aeaeec19e541c45605
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-25 10:13:01 -0300

    feat: criação da pagina que lista produtos. Criamos o método index no controller, configuramos a rota. E geramos o template da view. php | sail artisan make:view produtos.index

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
     //
+    public function index()
+    {
+        //echo "Listagem de produtos";
+       $listaProdutos = Produto::all();
+        //dd($listaProdutos);
+        return view('produtos.index', ['produtos' => $listaProdutos]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,5 @@ Route::get('/', function () {
 Route::get('/ola', [HomeController::class,'index']);
 Route::get('/ola/{name}', [HomeController::class,'index']);
 Route::get('/users', [HomeController::class,'list']);
+
+Route::get('/produtos', [ProdutoController::class,'index']);
