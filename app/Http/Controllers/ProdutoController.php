c90339ee11344c9a7d97dcb9704f54c999f9d1d7
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-25 10:54:27 -0300

    feat: criação dos métodos crud para show, store e create.

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -11,8 +11,31 @@ class ProdutoController extends Controller
     public function index()
     {
         //echo "Listagem de produtos";
-       $listaProdutos = Produto::all();
+       $listaProdutos = Produto::all()->reverse();
         //dd($listaProdutos);
         return view('produtos.index', ['produtos' => $listaProdutos]);
+    }
+
+    public function show($id)
+    {
+        // $produto = Produto::findOrFail($id);//Gera exceção e a página de 404
+        $produto = Produto::find($id);
+        return view('produtos.show', ['produto' => $produto]);
+    }
+
+    public function store(Request $request)
+    {
+        $novoProduto = $request->all();
+        $novoProduto['importado'] = $request->has('importado');
+
+        if(Produto::create($novoProduto)){
+            return redirect('/produtos');
+        }
+       dd("Erro ao criar produto!");
+    }
+
+    public function create()
+    {
+        return view('produtos.create');
     }
 }
