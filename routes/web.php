b46badc53b8eb05f79a2d417aa6c9e4925abe44f
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-04-15 08:31:50 -0300

    feat: exemplos agrupamento de rotas por prefixo

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/ola', [HomeController::class,'index']);
-Route::get('/ola/{name}', [HomeController::class,'index']);
-Route::get('/users', [HomeController::class,'list']);
+Route::get('/ola', [HomeController::class, 'index']);
+Route::get('/ola/{name}', [HomeController::class, 'index']);
+Route::get('/users', [HomeController::class, 'list']);
 
-Route::get('/produtos', [ProdutoController::class,'index']);
-Route::get('/produtos/{id}', [ProdutoController::class,'show']);
-Route::get('/produto', [ProdutoController::class,'create']);
-Route::post('/produto', [ProdutoController::class,'store']);
 
-Route::get('/produto/{id}/edit', [ProdutoController::class,'edit']);
-Route::post('/produto/{id}/update', [ProdutoController::class,'update'])->name('produto.update');
 
-Route::get('/produto/{id}/delete',[ProdutoController::class,'delete']);
-Route::post('/produto/{id}/remove', [ProdutoController::class,'remove'])->name('produto.remove');
+Route::prefix('produtos')->group(function () {
+    Route::get('/', [ProdutoController::class, 'index']);
+    Route::get('/{id}', [ProdutoController::class, 'show']);
+});
+
+Route::prefix('produtos')->group(function () {
+    Route::get('/', [ProdutoController::class, 'create']);
+    Route::post('/', [ProdutoController::class, 'store']);
+    Route::get('/{id}/edit', [ProdutoController::class, 'edit']);
+    Route::post('/{id}/update', [ProdutoController::class, 'update'])->name('produto.update');
+    Route::get('/{id}/delete', [ProdutoController::class, 'delete']);
+    Route::post('/{id}/remove', [ProdutoController::class, 'remove'])->name('produto.remove');
+});
