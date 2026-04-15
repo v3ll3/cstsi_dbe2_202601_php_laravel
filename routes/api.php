ebcc1302754041f2985b3b136dfb88f63b23c576
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-04-15 10:33:07 -0300

    feat: instalação dos arquivos de API. Comando configura o framework e instala as dependências para api. Cria os arquivos route/api.php, config/sanctum, migrations de personal tokens e outras configurações. php/sail artisan install:api

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Api\FornecedorController;
+use App\Http\Controllers\Api\ProdutoController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
