95779047ba4c18cc1abb1c4707a4392fa88a929e
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-04-15 10:37:23 -0300

    feat: registra a rota para o controlador de fornecedor da api. Todas as rotas na api possuem por padrão o prefixo api/. Para visualizar as rotas de api ustilize o comando: sail/php artisan route:list --path=api/fornecedores

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('fornecedores', FornecedorController::class)
+    ->parameters(["fornecedores" => 'fornecedor']);
+
