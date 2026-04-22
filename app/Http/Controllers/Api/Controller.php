d12b6ad68aaebd5ed5b6912e3f7d2b5ce214d98a
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-04-22 13:33:46 -0300

    refactor: modifica o método errorHandler. Passa toda a lógica de geração de saída json para a classe específica ExceptionJsonResponse.

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -2,24 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exceptions\ExceptionJsonResponse;
 use Exception;
 
 class Controller
 {
     protected function errorHandler(string $message, Exception $error)
     {
-        $httpStatus = 500;
-        $response = [
-            "data" => [
-                "message" => $message,
-                "error" => true,
-            ]
-        ];
-        if (env("APP_DEBUG"))
-            $response['data'] = [
-                "message" => $error->getMessage(),
-                "trace" => $error->getTrace()
-            ];
-        return  response()->json($response, $httpStatus);
+        throw new ExceptionJsonResponse($message,previous:$error);
     }
 }
