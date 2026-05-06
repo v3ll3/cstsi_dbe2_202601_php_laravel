f5f67b28c4d75b5373a250528b2ce16904d871ca
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-05-06 09:02:08 -0300

    refactor: exemplos de rotas na web

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\FornecedorController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,14 +22,14 @@ Route::controller(ProdutoController::class)->group(function () {
     });
 
     // Route::middleware('auth')->group(function() {
-        Route::prefix('produto')->group(function () {
-                Route::get('/',  'create');
-                Route::post('/',  'store');
-                Route::get('/{id}/edit',  'edit');
-                Route::post('/{id}/update',  'update')->name('produto.update');
-                Route::get('/{id}/delete',  'delete');
-                Route::post('/{id}/delete',  'remove')->name('produto.remove');
-            });
+    Route::prefix('produto')->group(function () {
+        Route::get('/',  'create');
+        Route::post('/',  'store');
+        Route::get('/{id}/edit',  'edit');
+        Route::post('/{id}/update',  'update')->name('produto.update');
+        Route::get('/{id}/delete',  'delete');
+        Route::post('/{id}/delete',  'remove')->name('produto.remove');
+    });
     // });
 });
 
@@ -38,5 +39,15 @@ Route::controller(ProdutoController::class)->group(function () {
 //     echo "Realize o Login!!!";
 // })->name('login');
 
-Route::resource('fornecedores',FornecedorController::class)
-    ->parameters(["fornecedores"=>'fornecedor']);
+Route::resource('fornecedores', FornecedorController::class)
+    ->parameters(["fornecedores" => 'fornecedor']);
+
+// Route::get('usuarios',[UsuarioController::class,'index'])->name('usuarios.index');
+// Route::post('usuarios',[UsuarioController::class,'store'])->name('usuarios.store');
+// Route::get('usuarios/create',[UsuarioController::class,'create'])->name('usuarios.create');
+// Route::get('usuarios/{usuario}',[UsuarioController::class,'show'])->name('usuarios.show');
+// Route::put('usuarios/{usuario}',[UsuarioController::class,'update'])->name('usuarios.update');
+// Route::delete('usuarios/{usuario}',[UsuarioController::class,'delete'])->name('usuarios.destroy');
+// Route::get('usuarios/{usuario}/edit',[UsuarioController::class,'edit'])->name('usuarios.edit');
+
+Route::resource('usuarios', UsuarioController::class);
