4dae7e181b3e85594a7904d863897f2427190fa0
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-05-13 11:16:31 -0300

    refactor: config sanctum stateful api. Neste caso o sanctum também suporta cookies.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend:[ForceJsonResponse::class]);
+        $middleware->statefulApi();
         // dd($middleware->getMiddlewareGroups());
     })
     ->withExceptions(function (Exceptions $exceptions): void {
