b504c298a2233d498427245a8bffb945b4dd4850
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-05-13 11:11:45 -0300

    feat: controller para recurso de User. Implementação dos métodos store e index. Store para registro/cadastro de novos usuários. Index para testar acesso somente autenticado.

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\Controller;
+use App\Http\Requests\UserStoreRequest;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\UserResourceCollection;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new UserResourceCollection(User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserStoreRequest $request)
+    {
+         try {
+            return new UserResource(User::create($request->validated()))
+                ->additional(["message"=>"Usuário cadastrado!!"])
+                ->response()->setStatusCode(201);
+        } catch (\Exception $error) {
+            return $this->errorHandler("Erro ao cadastrar o usuário!!!", $error);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
