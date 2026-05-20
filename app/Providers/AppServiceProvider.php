48fe88c60a8b462bedef1d3441968d29ca9c91d6
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-05-20 11:40:23 -0300

    feat: criação de gates para aplicação de autorizações

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('update-user',function (User $user, User $model) {
+                // dd([$user,$model]);
+                return $user->id === $model->id;
+        });
+
+        Gate::define('superuser',function(User $user){
+            return $user->is_admin;
+        });
     }
 }
