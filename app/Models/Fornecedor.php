3c0faf7fe61efe3f59b1ef507fa5619f4f284c87
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-06-03 10:55:44 -0300

    refactor: relações de fornecedor com produto. Mapeamento objeto relacional com a classe de produtos. As configurações  da model utilizam atributos.

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -2,15 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Table;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+#[Table('fornecedores')]
+#[Fillable([
+    "nome",
+    "cnpj",
+    "email",
+    "estado_id",
+    "telefone",
+    "endereco"
+])]
 class Fornecedor extends Model
 {
-    //
+    use HasFactory;
 
-    public function produtos() {
-        return $this->hasMany(Produto::class);
+    public function estado()
+    {
+        return $this->belongsTo(Estado::class);
     }
 
-  //$fornecedorModel->produtos
+    public function produtos()
+    {
+        return $this->hasMany(Produto::class);
+    }
 }
