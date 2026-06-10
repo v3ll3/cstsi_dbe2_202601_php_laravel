c8ebae75fd17f571df0a1c7866ba93fb347c149f
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-06-10 13:12:22 -0300

    refactor: relacionamento indireto com fornecedores e profundo com produtos. Uso do método hasManyThrough do Eloquent para mapear a relação indireta de região com fornecedor. Uso do pacote hasManyDeep para mapear a relação com produtos através de Fornecedor e Estado.

--- app/Models/Regiao.php
+++ app/Models/Regiao.php
@@ -6,10 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Regiao extends Model
 {
+    use  \Staudenmeir\EloquentHasManyDeep\HasRelationships;
+
     protected $table = 'regioes';
     protected $fillable = ['nome'];
 
-    public function estados(){
+    public function estados()
+    {
         return $this->hasMany(Estado::class);
+    }
+
+    public function fornecedores()
+    {
+        return $this->hasManyThrough(Fornecedor::class, Estado::class);
+    }
+
+      public function produtos()
+    {
+        return $this->hasManyDeep(
+            Produto::class,
+            [Estado::class,Fornecedor::class]
+        );
     }
 }
