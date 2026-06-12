f5130389b8280218cec5310cb6929b783ec5e91d
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-06-12 11:31:54 -0300

    fix: protege rota de cadastro de usuário com cookies xsrf

--- routes/api.php
+++ routes/api.php
@@ -16,39 +16,41 @@ Route::get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function () {
 
-    Route::get('produtos/filter/', [ProdutoController::class,'filter']);
+    Route::get('produtos/filter/', [ProdutoController::class, 'filter']);
 
     Route::apiResource('produtos', ProdutoController::class);
 
     Route::apiResource('fornecedores', FornecedorController::class)
         ->parameters(["fornecedores" => 'fornecedor']);
 
-    Route::apiResource('users', UserController::class)
-        ->only(['store']);
+
 
     Route::middleware('web')->group(function () {
 
-    Route::get('user',[LoginController::class,'user'])->middleware('auth:sanctum');
-    Route::post('logout',[LoginController::class,'logout'])->middleware('auth:sanctum');
+        Route::apiResource('users', UserController::class)
+            ->only(['store']);
+
+        Route::get('user', [LoginController::class, 'user'])->middleware('auth:sanctum');
+        Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 
         Route::apiResource('users', UserController::class)
             ->only(['index', 'show', 'update'])
             ->middleware('auth:sanctum');
 
-        Route::apiResource('users',UserController::class)
+        Route::apiResource('users', UserController::class)
             ->only('destroy')
-            ->middleware(['auth:sanctum','ability:is_admin']);
+            ->middleware(['auth:sanctum', 'ability:is_admin']);
 
         Route::apiResource('produtos', ProdutoController::class)
-            ->except(['index','show'])
+            ->except(['index', 'show'])
             ->middleware('auth:sanctum');
         Route::post('login', [LoginController::class, 'login']);
     });
 
-    Route::prefix('token')->group(function ()  {
-        Route::post('login',[LoginTokensController::class,'login']);
+    Route::prefix('token')->group(function () {
+        Route::post('login', [LoginTokensController::class, 'login']);
         // Route::post('logout',[LoginTokensController::class,'login']);
-        Route::get('user',function (Request $request){
+        Route::get('user', function (Request $request) {
             return $request->user();
         })->middleware('auth:sanctum');
     });
