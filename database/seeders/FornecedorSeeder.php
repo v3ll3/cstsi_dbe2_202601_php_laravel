9ec939ad71d33e635627a8eb6e968b915e18c582
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-06-17 16:11:43 -0300

    refactor: conifgura e aplica factories de fornecedor com produtos, promocoes e links.

--- database/seeders/FornecedorSeeder.php
+++ database/seeders/FornecedorSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Fornecedor;
+use App\Models\Link;
 use App\Models\Produto;
+use App\Models\Promocao;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,7 +18,21 @@ class FornecedorSeeder extends Seeder
     public function run(): void
     {
         Fornecedor::factory(10)
-        ->has(Produto::factory(50))
-        ->create();
+            ->has(
+                Produto::factory(50)
+                    ->hasAttached(
+                        Promocao::factory()->count(3),
+                        [
+                            'created_at' => Carbon::now()->toDateTimeString(),
+                            'updated_at' => Carbon::now()->toDateTimeString(),
+                            'desconto' => fake()->numberBetween(10, 50)
+                        ],
+                        'promocoes'
+                    )
+                    ->hasLinks(2)
+            )
+            ->has(Link::factory(2))
+            ->hasProdutos(2)
+            ->create();
     }
 }
