d97187ce351fdd21a36fc2039a84f4d1db51fa8c
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-06-17 08:33:51 -0300

    feat: metodo para lista de fornecedores

--- app/Http/Controllers/Api/FornecedorController.php
+++ app/Http/Controllers/Api/FornecedorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Fornecedor;
 use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
@@ -12,7 +13,8 @@ class FornecedorController extends Controller
      */
     public function index()
     {
-        //
+
+        return Fornecedor::all();
     }
 
     /**
