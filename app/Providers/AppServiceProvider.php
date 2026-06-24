0164143af543f9d3fb27ac5ae22e381d70264f53
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-06-24 09:25:30 -0300

    chore: config https ratelimiting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +32,20 @@ class AppServiceProvider extends ServiceProvider
 
         Gate::define('superuser',function(User $user){
             return $user->is_admin;
+        });
+
+        //Configura limite de requisições para a API
+        $this->configureRateLimiting();
+         if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
+
+    //Para limitar requisições na API por usuário ou IP
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
     }
 }
