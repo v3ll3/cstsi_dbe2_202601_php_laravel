1ec5123d437491c6dd505b76a387166bcd0caf6f
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-07-01 16:32:11 -0300

    feat: adiciona rota protegida enquando valida autenticação

--- resources/js/config/PrivateRoute.jsx
+++ resources/js/config/PrivateRoute.jsx
@@ -1,8 +1,48 @@
 /* eslint-disable react/prop-types */
-import { Navigate } from "react-router";
+
+import { useEffect, useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router";
 import { useAuthContext } from "../contexts/AuthProvider";
+import appLogo from "../assets/appLogo.svg";
+
+import { DefaultLogo, DefaultStyled, LogoutIcon } from "../layouts/layouts.styled";
 
 export default function PrivateRoute({ children }){
-  const { token } = useAuthContext();
-  return token ? children : <Navigate to="/login" />;
-};
+
+  const { isLogged } = useAuthContext();
+
+  if (!isLogged) return <Navigate to="/login" />;
+  if (isLogged instanceof Promise) return <Protected>{children}</Protected>
+  return children;
+}
+
+export const Protected = ({ children }) => {
+  const { isLogged } = useAuthContext();
+  const [isProtected, setIsProtected] = useState(false)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isLogged) navigate('/login')
+    if (isLogged === true) setIsProtected(isLogged)
+  }, [isLogged])
+
+  return (
+    isProtected
+      ? children
+      : <DefaultStyled>
+        <header>
+          <DefaultLogo>
+            <Link href="/">
+              <img src={appLogo} />
+            </Link>
+          </DefaultLogo>
+          <div>Dashboard</div>
+        </header>
+        <main>
+          <section>
+            <p>Verificando autenticação...</p>
+          </section>
+        </main>
+      </DefaultStyled>
+  );
+}
